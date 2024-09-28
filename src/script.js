9996de3567d9c8127879519bca150d827a9ea6df
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-09-28 16:39:27 +0500

    chng made

--- src/script.js
+++ src/script.js
@@ -28,16 +28,13 @@ workToggle.addEventListener('click', function (e) {
 
 
 
-let appendNumber = 600;
+let appendNumber = 20;
 let prependNumber = 1;
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
+  slidesPerView: 2,
   centeredSlides: true,
   spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-  },
+ 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -45,7 +42,7 @@ const swiper = new Swiper('.swiper', {
   virtual: {
     slides: (function () {
       const slides = [];
-      for (var i = 0; i < 600; i += 1) {
+      for (var i = 0; i < 20; i += 1) {
         slides.push('Slide ' + (i + 1));
       }
       return slides;
@@ -60,29 +57,6 @@ document
     swiper.slideTo(0, 0);
   });
 
-document
-  .querySelector('.slide-250')
-  .addEventListener('click', function (e) {
-    e.preventDefault();
-    swiper.slideTo(249, 0);
-  });
-
-document
-  .querySelector('.slide-500')
-  .addEventListener('click', function (e) {
-    e.preventDefault();
-    swiper.slideTo(499, 0);
-  });
-
-document
-  .querySelector('.prepend-2-slides')
-  .addEventListener('click', function (e) {
-    e.preventDefault();
-    swiper.virtual.prependSlide([
-      'Slide ' + --prependNumber,
-      'Slide ' + --prependNumber,
-    ]);
-  });
 
 document
   .querySelector('.append-slide')
