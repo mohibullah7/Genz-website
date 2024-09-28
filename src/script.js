784a321db09c8fb421b84169fd317c5b76a9c9e9
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-09-28 15:54:40 +0500

    today work

--- src/script.js
+++ src/script.js
@@ -26,3 +26,67 @@ workToggle.addEventListener('click', function (e) {
 });
 
 
+
+
+let appendNumber = 600;
+let prependNumber = 1;
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  virtual: {
+    slides: (function () {
+      const slides = [];
+      for (var i = 0; i < 600; i += 1) {
+        slides.push('Slide ' + (i + 1));
+      }
+      return slides;
+    })(),
+  },
+});
+
+document
+  .querySelector('.slide-1')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    swiper.slideTo(0, 0);
+  });
+
+document
+  .querySelector('.slide-250')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    swiper.slideTo(249, 0);
+  });
+
+document
+  .querySelector('.slide-500')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    swiper.slideTo(499, 0);
+  });
+
+document
+  .querySelector('.prepend-2-slides')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    swiper.virtual.prependSlide([
+      'Slide ' + --prependNumber,
+      'Slide ' + --prependNumber,
+    ]);
+  });
+
+document
+  .querySelector('.append-slide')
+  .addEventListener('click', function (e) {
+    e.preventDefault();
+    swiper.virtual.appendSlide('Slide ' + ++appendNumber);
+  });
